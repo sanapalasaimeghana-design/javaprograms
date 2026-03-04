fa109b3107f3a68453fef87a3ab64db563437e1d
sanapalasaimeghana-design <sanapalasaimeghana@gmail.com>
2026-03-04 21:26:37 +0530

    Add files via upload

---
+++ studentage.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class studentage
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a name,age");
+String name=sc.nextLine();
+int age=sc.nextInt();
+System.out.println(name);
+System.out.println(age);
+}
+}
