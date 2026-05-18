769f49034bcce8a6a17759c762f59c35031a75a8
sanapalasaimeghana-design <263928565+sanapalasaimeghana-design@users.noreply.github.com>
2026-05-18 15:15:54 +0530

    stringmethodes.java is added

---
+++ stringmethodes.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class stringmethodes
+{
+public static void main(String args[])
+{
+String str="hello meghana";
+System.out.println("uppercase:"+str.toUpperCase());
+System.out.println("lowercase:"+str.toLowerCase());
+System.out.println("length:"+str.length());
+System.out.println("concatination:"+str.concat("! welcome"));
+System.out.println("position:"+str.charAt(0));
+System.out.println("index:"+str.indexOf('m'));
+}
+}
+
+
+
+
+
