1db862894f6478d190dfc4e446420bd2a925e8fd
sanapalasaimeghana-design <263928565+sanapalasaimeghana-design@users.noreply.github.com>
2026-05-18 15:46:08 +0530

    recursivittrrativefact.java is added

---
+++ recursivittrrativefact.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class recursivittiretivefact
+{
+ class resursivefact(int num);
+  if(n==1 || n==0)
+  return 1;
+  else
+  return num*resursive(num-1)
+}
+
+{
+class ittiretivefact(int num);
+   if(n==1||n==0)
+   return 1;
+   else
+   { 
+    fact=1;
+    for(int i=1;i<=n;i++)
+    {
+     fact=fact*i;
+    }
+return fact;
+   }
+ }
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter n value");
+int n=sc.nextInt();
+recursivfact obj=new recursivfact(n);
+System.out.println(
+}
+
