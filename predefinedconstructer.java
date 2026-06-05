9ad05be2510f48b6ede066a66ece35ef71035d67
sanapalasaimeghana-design <sanapalasaimeghana@gmail.com>
2026-06-05 14:56:04 +0530

    predefinedconstructer.java is added

---
+++ predefinedconstructer.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class predefinedconstructer
+{
+int age;
+String name;
+predefinedconstructer()
+{
+age=19;
+name="meghana";
+}
+public static void main(String args[])
+{
+predefinedconstructer obj=new predefinedconstructer();
+System.out.println("age:"+obj.age+"name:"+obj.name);
+}
+}
